0528845f23a0d463037f6b5ffa6671ab1c2df840
prathyu <melamprathyusha@gmail.com>
2024-04-26 15:07:58 +0530

    Added ExceptionHandler class for accepting general exceptions

---
+++ src/main/java/com/mobileAppDev/webserives/MobileApp/ui/model/exceptions/AppExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.mobileAppDev.webserives.MobileApp.ui.model.exceptions;
+
+import com.mobileAppDev.webserives.MobileApp.UserControllers.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value={Exception.class})
+    public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
+
+        String errorMsg = ex.getLocalizedMessage();
+        if(errorMsg==null) errorMsg = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(),errorMsg);
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR) ;
+    }
+
+
+}
