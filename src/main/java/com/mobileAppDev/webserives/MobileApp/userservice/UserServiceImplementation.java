8da88e99dcbb3def470cd3b30ec5f5225bcced09
prathyu <melamprathyusha@gmail.com>
2024-04-26 17:04:19 +0530

    Added Constructor based Dependency Injection

--- src/main/java/com/mobileAppDev/webserives/MobileApp/userservice/UserServiceImplementation.java
+++ src/main/java/com/mobileAppDev/webserives/MobileApp/userservice/UserServiceImplementation.java
@@ -2,6 +2,8 @@ package com.mobileAppDev.webserives.MobileApp.userservice;
 
 import com.mobileAppDev.webserives.MobileApp.ui.model.request.UserDetailsRequestModel;
 import com.mobileAppDev.webserives.MobileApp.ui.model.response.UserRest;
+import com.mobileAppDev.webserives.MobileApp.userservice.shared.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -12,6 +14,14 @@ import java.util.UUID;
 public class UserServiceImplementation implements UserService {
 
     Map<String, UserRest> users;
+    Utils utils;
+
+    public UserServiceImplementation() {}
+
+    @Autowired
+    public UserServiceImplementation(Utils utils) {
+        this.utils = utils;
+    }
 
     @Override
     public UserRest createUser(UserDetailsRequestModel userDetails) {
@@ -20,7 +30,7 @@ public class UserServiceImplementation implements UserService {
         user.setLastName(userDetails.getLastName());
         user.setEmail(userDetails.getEmail());
 
-        String userId= UUID.randomUUID().toString();
+        String userId= utils.generateUserId();
         user.setUserId(userId);
         if(users==null) users = new HashMap<>();
         users.put(userId, user);
