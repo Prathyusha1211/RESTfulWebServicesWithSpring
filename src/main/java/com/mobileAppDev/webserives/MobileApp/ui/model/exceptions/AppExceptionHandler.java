0d4fffb92a79c647f6556683068fe0f05003fb8b
prathyu <melamprathyusha@gmail.com>
2024-04-26 15:37:15 +0530

    Edited method in ExceptionHandler class for handling multiple exceptions in one method

--- src/main/java/com/mobileAppDev/webserives/MobileApp/ui/model/exceptions/AppExceptionHandler.java
+++ src/main/java/com/mobileAppDev/webserives/MobileApp/ui/model/exceptions/AppExceptionHandler.java
@@ -21,21 +21,14 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR) ;
     }
 
-    @ExceptionHandler(value={NullPointerException.class})
-    public ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request){
+    @ExceptionHandler(value={NullPointerException.class,UserServiceException.class})
+    public ResponseEntity<Object> handleSpecificExceptions(Exception ex, WebRequest request){
         String errorMsg = ex.getLocalizedMessage();
         if(errorMsg==null) errorMsg = ex.toString();
         ErrorMessage errorMessage = new ErrorMessage(new Date(),errorMsg);
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR) ;
     }
 
-    @ExceptionHandler(value={UserServiceException.class})
-    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
-        String errorMsg = ex.getLocalizedMessage();
-        if(errorMsg==null) errorMsg = ex.toString();
-        ErrorMessage errorMessage = new ErrorMessage(new Date(),errorMsg);
-        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR) ;
-    }
 
 
 }
